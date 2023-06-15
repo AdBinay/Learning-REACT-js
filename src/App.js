05e417afbe8b322a8057c65a6ee6a9fc0cf39ac7
AdBinay <binayadh77@gmail.com>
2023-06-15 09:39:50 +0545

    Till video 12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"/>
-      <div className="container my-3">
-        <TextForm />
-        {/* <About/> */}
-      </div>
-    </>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    </div>
+    </> 
   );
 }
-
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,58 +1,62 @@
-import React , {useState} from 'react'
-import PropTypes from 'prop-types'
-
-
-
+import React, {useState} from 'react'
 export default function TextForm(props) {
-   const handleUpClick = ()=>{
-    let newText = text.toUpperCase();
-    setText(newText)
-   }
-   const handleLowClick = ()=>{
-    let newText = text.toLowerCase();
-    setText(newText)
-   }
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText)
+    }
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value)
+    }
+    // Credits: A
+    const handleCopy = () => {
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+    }
+    // Credits: Coding Wala
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "))
-}
-   
-    const handleClearClick = ()=>{
-    let newText ='';
-    setText(newText)
-   }
-
-   const handleOnChange = (event)=>{
-    setText(event.target.value);
-   }
-
-  const[text, setText] = useState(' ');
-  
-  return (
-    <>
-    <div>
-        <div className="container mb-3">
-            <label htmlFor="my-box" className="form-label"><h2>{props.heading}</h2></label>
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
-        </div>
-        <button className="btn btn-primary mx-2"  onClick={handleUpClick} type="button">convert to uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}  type="button">convert to lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}  type="button">clear</button>
-        <button className="btn btn-primary mx-2 my-3" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-    </div>
-    <div className="container my-3">
-        <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words , {text.length} characters</p>
-        <p>{0.008*text.split(" ").length} Minutes read</p>
-        <h3>Preview</h3>
-        <p>{text}</p>
-    </div>
-    </>
-  )
-}
-TextForm.propTypes={
-    heading: PropTypes.string.isRequired, 
-}
-TextForm.defaultProps={
-    heading:"Your Text Here For Analysis.",
+    }
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+            <div className="container"> 
+                <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+                    <h1>{props.heading}</h1>
+                    <div className="mb-3"> 
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+                    </div>
+                    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                    <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                </div>
+            </div>
+            <div className="container my-3">
+                <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+                    <h2>Your text summary</h2>
+                    <p>{text.split(" ").length} words and {text.length} characters</p>
+                    <p>{0.008 *  text.split(" ").length} Minutes read</p>
+                    <h2>Preview</h2>
+                    <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                </div>
+            </div>
+        </>
+    )
 }
